56caad650402c8d6c7fd5597fdb23fc0ca8292a9
ayathree <nobanitaayathree333@gmail.com>
2024-03-09 03:36:26 +0600

    added userid

--- src/Post.jsx
+++ src/Post.jsx
@@ -2,10 +2,10 @@ import './Post.css'
 
 export default function Post({post}){
     console.log(post)
-    const {body, userID, id, title }= post
+    const {body, userId, id, title }= post
     return(
         <div className='body'>
-            <p>userID: {userID} </p>
+            <p>userID: {userId} </p>
             <p>id: {id} </p>
             <p>title: {title} </p>
             <p>body: {body} </p>
